6596555ce3faf60a3b93e2e3f11733813e7649d2
nP0mmeOctSbkiA <samira.ancarola@gmail.com>
2025-01-14 10:00:16 +0100

    Remove unused Preact import and update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
     site: "https://test1-astro.netlify.app"
